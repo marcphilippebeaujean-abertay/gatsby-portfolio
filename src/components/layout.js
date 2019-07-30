11ad65c5e966f30b9480c690f11e15236d91b242
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-07-30 21:10:08 +0200

    added global style

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,19 @@
-import React from "react"
+import React from "react";
 import MainMenu from "./mainMenu";
 import Footer from "./footer";
 
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+  body{
+    font-family: 'Open Sans', sans-serif;
+  }
+`
+
 const Layout = ({ children }) => (
   <div>
+    <GlobalStyles />
     <MainMenu />
     {children}
     <Footer />
