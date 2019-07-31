743f6b66058c4118926745e26791f0dd38059d7e
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-07-31 21:25:26 +0200

    redesign for nav elements

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,23 @@
 import React from "react";
 import NavMenu from "./navMenu";
 import Footer from "./footer";
-import { GlobalStyles, ContentLayoutWrapper } from "../style/layoutStyle";
+import { navMenuHeight, contentWidth } from  "../style/layoutStyle";
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+  body{
+    font-family: 'Open Sans', sans-serif;
+    padding: 0 !important;
+    display: block;
+    margin-top: ${navMenuHeight}px;
+  }
+`
+
+export const ContentLayoutWrapper = styled.div`
+  max-width: ${contentWidth}px;
+  margin: 0 auto;
+`
 
 const Layout = ({ children }) => (
   <div>
--- src/components/navMenu.js
+++ src/components/navMenu.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import {graphql, useStaticQuery } from 'gatsby';
-import { MainMenuWrapper, MenuItem } from  "../style/navMenuStyle";
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import { navMenuHeight, contentWidth } from  "../style/layoutStyle";
+import { mainColour } from "../style/themeStyle";
+import styled from "styled-components";
+
+const navMenuPartitions = 6;
+const navMenuWidth = contentWidth / navMenuPartitions;
+
+export const MainMenuWrapper = styled.nav`
+    background-color: rgb(0, 0, 0);
+    display: flex;
+    width: ${contentWidth}px;
+    position: fixed;
+    left: 50%;
+    transform: translateX(-50%);
+    margin: 0 auto;
+    padding: 0;
+    top: 0%;
+    border-radius: 0px 0px 10px 10px;
+`
+
+export const MenuItem = styled(Link)`
+    color: ${mainColour};
+    text-decoration: none;
+    height: ${navMenuHeight}px;
+    width: ${navMenuWidth}px;
+    text-align: center;
+    flex-direction: row;
+    display: table-cell;
+    vertical-align: middle;
+`
 
 const NavMenu = () => {
+    // language=GraphQL
     const data = useStaticQuery(graphql`
         query{
           allWordpressWpApiMenusMenusItems(filter: {
@@ -19,14 +49,14 @@ const NavMenu = () => {
                 }
               }
             }
-          } 
+          }
         }
     `)
     return (
     <MainMenuWrapper>
         {data.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item => (
             <MenuItem to={item.object_slug} key={item.title}>
-                {item.title}
+                <p>{item.title}</p>
             </MenuItem>
         ))}
     </MainMenuWrapper>
