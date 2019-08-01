5de8de8a173675e271abfd66c3ff63508c13f09f
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-08-01 14:49:15 +0200

    initial logo design with svg

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { mainColour } from "../style/themeStyle";
 import styled from "styled-components";
 
 const LogoStyle = styled.div`
@@ -6,23 +7,22 @@ const LogoStyle = styled.div`
     display: table-cell;
     vertical-align: middle;
     width: ${props => (100 / props.menuPartitions) * 2}%;
-    margin-top: 10px;
 `
 
-const LogoText = styled.p`
+const LogoText = styled.text`
     @import url('https://fonts.googleapis.com/css?family=Orbitron&display=swap');
     font-family: 'Orbitron', sans-serif;
-    margin: 0;
-    font-weight: bold;
-    text-align: justify;
-    font-size: 16px;
-    text-align: center;
+    font-size: 160px;
+    fill: ${mainColour};
 `
 
 
 const Logo = (props) => (
     <LogoStyle id="logo" menuPartitions={props.menuPartitions}>
-        <LogoText>{`<JustDoIT />`}</LogoText>
+        <svg height="100%" width="100%" viewBox="0 -200 1000 300"
+             xmlns="http://www.w3.org/2000/svg">
+            <LogoText x="0%" y="0%" textLength="100%">{`<JustDoIT />`}</LogoText>
+        </svg>
     </LogoStyle>
 )
 
