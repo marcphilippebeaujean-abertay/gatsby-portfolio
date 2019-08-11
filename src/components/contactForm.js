e0513279b467b6599f5d435ea429ba40d62b8cb4
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-08-11 08:29:04 +0200

    implemented netlify forms, need to test

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { mainColour } from "../style/themeStyle";
 import { smallScreenWidth } from "../style/layoutStyle";
-import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
 
 const ContactsForm = styled.form`
     width: 100%;
@@ -22,8 +21,7 @@ const ContactsForm = styled.form`
         border-style: solid;
         box-shadow: 0px;
     }
-    .submit-btn{
-        margin: 20px 0px;
+    #submit-btn{
         width: 200px;
         height: 50px;
         color: ${mainColour};
@@ -34,39 +32,38 @@ const ContactsForm = styled.form`
         font-size: 16px;
         transition: all 0.3s;
     }
-    .submit-btn:hover{
+    #submit-btn:hover{
         color: black;
         background-color: ${mainColour};
         cursor: pointer;
     }
+    #recaptcha-token{
+        display: none;
+    }
     @media screen and (max-width: ${smallScreenWidth}px){
-        .submit-btn{
+        #submit-btn{
             width: 100%;
         }
     }
 `
 
 export default () => {
-    const { executeRecaptcha } = useGoogleReCaptcha();
-    const token = executeRecaptcha("send_email").then(promiseValue => console.log("captcha key is: "+promiseValue))
-                                                .catch(error => console.log("error retrieving captcha: "+error));
     return(
-        <div>
-            <ContactsForm>
-                <label htmlFor="name">
-                    Name:
-                </label>
-                <input className="input-field" type="text" name="your-name" placeholder="John Smith" />
-                <label htmlFor="email">
-                    E-Mail:
-                </label>
-                <input className="input-field" type="text" name="your-email" placeholder="example@mail.com" />
-                <label htmlFor="message">
-                    Message:
-                </label>
-                <textarea rows="10" className="input-field" name="your-message" placeholder="Hi Marc, I love your blog!" />
-                <input className="submit-btn" type="submit" value="Submit" />
-            </ContactsForm>
-        </div>
+        <ContactsForm name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
+            <label htmlFor="name">
+                Full Name:
+            </label>
+            <input className="input-field" id="form-name" type="text" name="name" placeholder="John Smith" />
+            <label htmlFor="email">
+                E-Mail:
+            </label>
+            <input className="input-field" id="form-email" type="text" name="email" placeholder="example@mail.com" />
+            <label htmlFor="message">
+                Message:
+            </label>
+            <label id="recaptcha-token">Don't fill out if you are human<input type="text" name="bot-field" /></label>
+            <textarea rows="10" id="form-message" className="input-field" name="message" placeholder="Hi Marc, I love your blog!" />
+            <input id="submit-btn" type="submit" value="Submit" disabled />
+        </ContactsForm>
     )
 }
