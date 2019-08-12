138d78d0be3f4488fdaabfd41f24244aa05bbc02
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-08-12 07:21:21 +0200

    now matching the users current url location correctly using regex

--- src/components/navMenu.js
+++ src/components/navMenu.js
@@ -63,7 +63,7 @@ const NavMenu = () => {
     }
   `);
   useEffect(()=>{
-    const currentUrl = document.location.pathname.slice(1);
+    const currentUrl = document.location.pathname.match(/^(\/)([a-z])*(?!\/)\w+/)[0].slice(1);
     const currentSelected = Array.from(document.getElementsByClassName("selected"));
     const newSelectedObject = Array.from(document.getElementsByClassName(currentUrl));
     if(currentSelected.length > 0) currentSelected.map(obj => obj.classList.remove("selected"));
