e7956604d61cb6a578df7ad1535a8e3169e7dde0
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-08-12 10:00:34 +0200

    added arrows to page navigation

--- src/templates/blogDisplayPage.js
+++ src/templates/blogDisplayPage.js
@@ -1,6 +1,7 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import PostPreview from "../components/postPreview";
 import styled from 'styled-components';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { Link } from "gatsby";
 import { PageContentWrapper } from './page';
 
@@ -10,8 +11,9 @@ const PageToggleStyle = styled.div`
   justify-content: center;
   align-content: center;
   margin-top: 10px;
+  color: black !important;
   a{
-    margin: 10px;
+    margin: 0 10px;
     text-decoration: none;
     font-size: 18px;
   }
@@ -21,39 +23,64 @@ const PageToggleStyle = styled.div`
   .bold{
     font-weight: bold;
   }
-  .bold a:hover{
-    text-decoration: none;
+  .bold:hover{
+    text-decoration: none !important;
+    cursor: default !important;
+  }
+  .inner-container{
+    display: flex;
+    position: relative;
+  }
+  #page-backward{
+    left: -40px;
+  }
+  #page-forward{
+    right: -40px;
+  }
+  .nav-arrow{
+    position: absolute;
+  }
+  svg{
+    margin-top: 2.6px;
   }
 `
 
 export default ({ pageContext }) => {
-    useEffect(() => {
-      let currentPageIndex = parseInt(document.location.pathname.match(/([1-9])(?!\/)$/)[0]);
-      const boldPageLink = document.getElementsByClassName('bold');
-      console.log(boldPageLink);
-      if(boldPageLink.length > 0) {
-        boldPageLink[0].classList.toggle('bold');
-      }
-      const pageLinkElement = document.getElementById(`page-link-${currentPageIndex}`);
-      pageLinkElement.classList.toggle('bold');
-    })
     return(
       <PageContentWrapper>
         <h1 style={{marginBottom: `0px`}} dangerouslySetInnerHTML={{__html: pageContext.title}} />
         {pageContext.posts.map(post => <PostPreview post={post.node}
                                                     key={post.node.title} />)}
         <PageToggleStyle>
-        {
-          pageContext.numberOfPages === 1 ? null :
+          <div className="inner-container">
+          {
+            pageContext.currentPage === 1 ? null : <Link to={`/blog/${pageContext.currentPage-1}`}
+                                                        className="nav-arrow"
+                                                        id="page-backward">
+                                                          <IoIosArrowBack size="20px" />
+                                                    </Link>
+          }
+          {
+            pageContext.numberOfPages === 1 ? null :
             Array.from({length: pageContext.numberOfPages}).map((page, index) => (
-            <div key={index}>
-              <Link to={`/blog/${index+1}`}
-                    id={`page-link-${index+1}`}>
-                {index + 1}
-              </Link>
-            </div>
-          ))
-        }
+              <div key={index}>
+                <Link to={`/blog/${index+1}`}
+                      id={`page-link-${index+1}`}
+                      className={pageContext.currentPage === index+1 ? `bold` : ``}>
+                  {index + 1}
+                </Link>
+              </div>
+            ))
+          }
+          {
+            pageContext.currentPage === pageContext.numberOfPages ? null : 
+                                                    <Link to={`/blog/${pageContext.currentPage+1}`}
+                                                          className="nav-arrow"
+                                                          id="page-forward">
+                                                          <IoIosArrowForward size="20px" />
+                                                    </Link>
+          }
+          </div>
         </PageToggleStyle>
       </PageContentWrapper>)
 }
