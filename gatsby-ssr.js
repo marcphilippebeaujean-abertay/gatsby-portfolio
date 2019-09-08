0e15002b376c550b8d0339539aa87f3b66f54650
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-09-08 10:03:30 +0200

    readded ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,17 @@
 
+const Layout = require("./src/components/layout").default
+const React = require("react")
+const { FacebookProvider } = require("react-facebook");
+
+//const { Helmet } = require("react-helmet")
+
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
+exports.wrapPageElement = ({ element, props }) => {
+    <Layout>
+        {element}
+    </Layout>
+}
