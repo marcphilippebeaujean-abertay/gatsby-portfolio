ae909c4f69965f7a57dea836043b11e38b3da31f
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-09-08 09:43:36 +0200

    removed ssr features

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,30 @@
 import React from "react"
 import { PageContentWrapper } from '../templates/page';
+import Img from "gatsby-image/withIEPolyfill";
+import { graphql, useStaticQuery } from 'gatsby';
 
 export default ({ pageContext }) => {
+  const data = useStaticQuery(graphql`
+    query {
+        errorPic: file(relativePath: { eq: "error-icon.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+                ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `);
   return (
     <PageContentWrapper>
       <h1>Error 404</h1>
-      <p><b>Oops</b>, we couldn't find this page!</p>
+      <div>
+        <div style={{width: `50%`}}>
+          <Img fluid={data.errorPic.childImageSharp.fluid} alt="Error picture!" />
+        </div>
+        <p><b>Oooops!</b> Looks like the page you were looking for cannot be accessed or doesn't exist. I apologize for the inconvenience 
+            - please contact me if you think this is a bug!</p>
+      </div>
     </PageContentWrapper>
   )
 }
