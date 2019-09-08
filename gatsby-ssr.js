b3929b0c2e87ced0d8e2106bbb526382cd2c407d
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-09-08 10:44:45 +0200

    removed layout from ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,9 +8,11 @@ const React = require("react")
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+
 exports.wrapPageElement = ({ element, props }) => {
     return (<Layout>
         {element}
     </Layout>)
 }
+
+ */
