7e74043ae50664ea93c16cc560615e81bbb6ffa3
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-09-09 19:50:46 +0200

    logo adjusted for firefox

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mainColour } from "../style/themeStyle";
-import { smallScreenWidth, smallScreenSidePadding } from "../style/layoutStyle";
+import { smallScreenWidth, smallScreenSidePadding, navMenuHeight } from "../style/layoutStyle";
 import styled from "styled-components";
 
 const LogoStyle = styled.div`
@@ -17,9 +17,14 @@ const LogoStyle = styled.div`
 `
 
 const LogoContainer = styled.div`
-    height: 100%;
+    height: 50px;
     width: 250px;
     margin: 0px auto;
+    @import url('https://fonts.googleapis.com/css?family=Orbitron&display=swap');
+    font-family: 'Orbitron', sans-serif;
+    :hover{
+        cursor: default;
+    }
     @media screen and (max-width: ${smallScreenWidth}px){
         vertical-align: left;
         width: 200px;
@@ -29,13 +34,7 @@ const LogoContainer = styled.div`
 `
 
 const LogoText = styled.text`
-    @import url('https://fonts.googleapis.com/css?family=Orbitron&display=swap');
-    font-family: 'Orbitron', sans-serif;
-    font-size: 160px;
-    fill: ${mainColour};
-    :hover{
-        cursor: default;
-    }
+    
 `
 
 const Logo = (props) => (
@@ -43,7 +42,7 @@ const Logo = (props) => (
         <LogoContainer>
             <svg y1="0%" y="100%" x1="0%" x2="100%" viewBox="0 -200 1000 300"
                  xmlns="http://www.w3.org/2000/svg">
-                <LogoText x="0%" y="0%" textLength="100%">{`<JustDoIT />`}</LogoText>
+                <text fontSize="153" fill={`${mainColour}`} letterSpacing="-0.5">{`<JustDoIT/>`}</text>
             </svg>
         </LogoContainer>
     </LogoStyle>
