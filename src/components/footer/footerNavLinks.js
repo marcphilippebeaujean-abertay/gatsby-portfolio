c114096eb83e56896c023fab7b94558e1948ea9e
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-10-30 06:52:05 +0100

    fixed missing key for footer nav links

--- src/components/footer/footerNavLinks.js
+++ src/components/footer/footerNavLinks.js
@@ -6,7 +6,7 @@ const topicLinks = ["Docker", "Linux", "DevOps", "Design", "JavaScript"]
 export default () => (
   <div className="d-flex flex-md-column flex-xs-row">
     {topicLinks.map(topic => (
-      <div className="link-wrap">
+      <div className="link-wrap" key={`${topic}-footer-nav-link`}>
         <Link
           className="footer-link"
           key={`${topic} footer link`}
