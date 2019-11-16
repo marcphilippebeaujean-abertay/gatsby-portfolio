1e11bef48b2b4d8b2e102dafdec8621ae7437de9
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-11-16 21:58:40 +0100

    made some stlying fixes for index page, padding on the featured articles and other refactoring

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,27 +16,6 @@ const PersonalPicWrapper = styled.div`
   overflow: hidden;
 `
 
-const HomePageStyles = styled.div`
-  .preview-img {
-    display: inline-block;
-    float: left;
-    width: 80px;
-  }
-  .read-more {
-    text-decoration: none;
-    transition: all 0.3s;
-  }
-  .read-more:hover {
-    text-decoration: underline;
-  }
-`
-
-const PostCard = ({ post }) => (
-  <Col md={6}>
-    <PostPreview post={post} showStats={false} />
-  </Col>
-)
-
 export default () => {
   const data = useStaticQuery(graphql`
     {
@@ -47,7 +26,7 @@ export default () => {
           }
         }
       }
-      allWordpressWpBlogpost(sort: { fields: date, order: DESC }, limit: 6) {
+      allWordpressWpBlogpost(sort: { fields: date, order: DESC }, limit: 4) {
         edges {
           node {
             slug
@@ -80,47 +59,49 @@ export default () => {
   })
   return (
     <PageContentWrapper>
-      <HomePageStyles>
-        <SEO
-          title={"Home"}
-          description={
-            "Hi! My name is Marc Philippe Beaujean and I'm a software developer from Germany."
-          }
-        />
-        <h1>Home</h1>
-        <hr></hr>
-        <Row>
-          <Col md={4}>
-            <PersonalPicWrapper>
-              <Img
-                fluid={data.personalPic.childImageSharp.fluid}
-                alt="Photo of the author"
-                className="wp-image-186"
-              />
-            </PersonalPicWrapper>
-          </Col>
-          <Col md={8} className="d-flex flex-column justify-content-center">
-            <h3>Welcome</h3>
-            <p>
-              At <b>ByteSchool</b>, I aim to offer high quality software
-              development and productivity articles to help you reach your
-              professional goals. If you like the content, please consider
-              subscribing to my newsletter to receive updates and exclusive
-              content!
-            </p>
-          </Col>
-        </Row>
-        <hr></hr>
-        <h2>Latest Articles</h2>
-        {posts.map((post, index) =>
-          index % 2 === 0 ? (
-            <Row key={post.title}>
-              <PostCard post={post} />
-              <PostCard post={posts[index + 1]} />
-            </Row>
-          ) : null
-        )}
-      </HomePageStyles>
+      <SEO
+        title={"Home"}
+        description={
+          "Hi! My name is Marc Philippe Beaujean and I'm a software developer from Germany."
+        }
+      />
+      <h1>Home</h1>
+      <hr></hr>
+      <Row>
+        <Col md={4}>
+          <PersonalPicWrapper>
+            <Img
+              fluid={data.personalPic.childImageSharp.fluid}
+              alt="Photo of the author"
+              className="wp-image-186"
+            />
+          </PersonalPicWrapper>
+        </Col>
+        <Col md={8} className="d-flex flex-column justify-content-center">
+          <h3>Welcome</h3>
+          <p>
+            At <b>ByteSchool</b>, I aim to offer high quality software
+            development and productivity articles to help you reach your
+            professional goals. If you like the content, please consider
+            subscribing to my newsletter to receive updates and exclusive
+            content!
+          </p>
+        </Col>
+      </Row>
+      <hr></hr>
+      <h2>Latest Articles</h2>
+      {posts.map((post, index) =>
+        index % 2 === 0 ? (
+          <Row key={post.title}>
+            <Col md={6} className="pr-md-1 pr-3 pl-3">
+              <PostPreview post={post} showStats={false} />
+            </Col>
+            <Col md={6} className="pl-md-1 pr-3 pl-3">
+              <PostPreview post={posts[index + 1]} showStats={false} />
+            </Col>
+          </Row>
+        ) : null
+      )}
     </PageContentWrapper>
   )
 }
