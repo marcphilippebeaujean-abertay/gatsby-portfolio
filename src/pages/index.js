917c527124f6d3482415afc413268fb26347b1b1
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2019-11-17 21:34:42 +0100

    updated the byteschool name in the welcoming message

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,11 +80,12 @@ export default () => {
         <Col md={8} className="d-flex flex-column justify-content-center">
           <h3 className="mt-0">Welcome</h3>
           <p className="mb-0">
-            I'm Marc, a software developer from Germany. At <b>ByteSchool</b>, I
-            aim to offer high quality software development and productivity
-            articles to help you reach your professional goals. If you like the
-            content, please consider subscribing to my newsletter to receive
-            updates and exclusive content!
+            I'm Marc, a software developer from Germany. At{" "}
+            <b>&lt;ByteSchool /&gt;</b>, I aim to offer high quality software
+            development and productivity articles to help you reach your
+            professional goals. If you like the content, please consider
+            subscribing to my newsletter to receive updates and exclusive
+            content!
           </p>
         </Col>
       </Row>
