ff6657ad3ec0cc8226aff218c89ab5225e8ca338
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-04-25 12:19:05 +0200

    adjusted link colour and reintroduced prism css

--- gatsby-config.js
+++ gatsby-config.js
@@ -226,8 +226,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        whitelist: ['.row', '.col', '.container'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: ['/src/style/prism.css'], // Ignore files/folders
         //purgeOnly: ['./node_modules/bootstrap/scss/bootstrap.scss/'], // Purge only these files/folders
       }
     },
