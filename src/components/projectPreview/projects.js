0d04a00284a23135144af3cffd76c8104eebeb8b
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-05-19 08:54:03 +0200

    added backlink to the moonlight game devs project

--- src/components/projectPreview/projects.js
+++ src/components/projectPreview/projects.js
@@ -22,32 +22,56 @@ export default () => {
           }
         }
       }
+      moonlightGameDevs: file(
+        relativePath: { eq: "icon_mgd.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 400) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }
   `)
   return (
-    <div className="row">
-      <div className="pr-md-1 pr-3 pl-3 col-md-6">
-        <ProjectPreview
-          projectTitle={"CastSpot [Alpha]"}
-          projectDescription={
-            "Platform that helps Podcasters grow their Audience and Network by featuring on each other's Shows."
-          }
-          projectLink={"https://castspot.io"}
-          imageData={data.castspotIcon.childImageSharp.fluid}
-        />
-      </div >
-      <div md={6} className="pl-md-1 pr-3 pl-3 col-md-6">
-        <ProjectPreview
-          projectTitle={"Django + React Cookiecutter"}
-          projectDescription={
-            "Kickstart your next Web App with this starter Project. Includes automated deployment to Heroku."
-          }
-          projectLink={
-            "https://github.com/marcphilippebeaujean-abertay/django-react-cookiecutter"
-          }
-          imageData={data.djangoReactCookiecutter.childImageSharp.fluid}
-        />
+    <React.Fragment>
+      <div className="row">
+        <div className="pr-md-1 pr-3 pl-3 col-md-6">
+          <ProjectPreview
+            projectTitle={"CastSpot [Alpha]"}
+            projectDescription={
+              "Platform that helps Podcasters grow their Audience and Network by featuring on each other's Shows."
+            }
+            projectLink={"https://castspot.io"}
+            imageData={data.castspotIcon.childImageSharp.fluid}
+          />
+        </div >
+        <div className="pl-md-1 pr-3 pl-3 col-md-6">
+          <ProjectPreview
+            projectTitle={"Django + React Cookiecutter"}
+            projectDescription={
+              "Kickstart your next Web App with this starter Project. Includes automated deployment to Heroku."
+            }
+            projectLink={
+              "https://github.com/marcphilippebeaujean-abertay/django-react-cookiecutter"
+            }
+            imageData={data.djangoReactCookiecutter.childImageSharp.fluid}
+          />
+        </div>
+        <div className="pr-md-1 pr-3 pl-3 col-md-6">
+          <ProjectPreview
+            projectTitle={"Moonlight Game Devs"}
+            projectDescription={
+              "Podcast and Articles on Game Development. Helping aspiring games founders succeed!"
+            }
+            projectLink={
+              "https://moonlightgamedevs.com"
+            }
+            imageData={data.moonlightGameDevs.childImageSharp.fluid} />
+        </div>
+        <div className="pl-md-1 pr-3 pl-3 col-md-6">
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   )
 }
